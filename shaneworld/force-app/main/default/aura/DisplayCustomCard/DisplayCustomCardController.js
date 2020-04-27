322d1dafa390c98fcb331e55d1c18f1b64285a2c
Akeem Shane <awshane420@gmail.com>
2020-04-27 00:34:51 -0400

    Finish Building community

--- shaneworld/force-app/main/default/aura/DisplayCustomCard/DisplayCustomCardController.js
+++ shaneworld/force-app/main/default/aura/DisplayCustomCard/DisplayCustomCardController.js
@@ -1,9 +1,9 @@
 ({
-	displayImage : function(component, event, helper) {
-		var card = component.get("v.cardData");
+    displayImage : function(component, event, helper) {
+        var card = component.get("v.cardData");
         var cardFrame = card["Card_Frame__c"];
-        var url = window.location.pathname + $A.get("$Resource." + cardFrame);
+        var url = "/sfsites/c/resource/" + cardFrame;
         console.log(url);
         component.find("cardFrameImg").set("v.src",url);
-	}
+    }
 })
